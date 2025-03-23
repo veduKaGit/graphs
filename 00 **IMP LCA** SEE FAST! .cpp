1a506157ea1e76f91b9b34cf4d6a6ad632a73ea2
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-23 12:31:15 +0530

    Update 00 **IMP LCA** SEE FAST! .cpp

--- 00 **IMP LCA** SEE FAST! .cpp
+++ 00 **IMP LCA** SEE FAST! .cpp
@@ -63,7 +63,7 @@ public:
 class Solution {
 public:
     // this function returns the LCA
-    // function returns NULL neither of p/q are present in subtree of root
+    // function returns NULL when NONE of p/q are present in subtree of root
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL)   //base case for NULL nodes
             return NULL;
