b16d3c1e62c6f863adf87e4a83d96c4fe479a70a
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-30 23:24:41 +0530

    Update 02.5 IMP.cpp

--- shortest_path_or_dist/02.5 IMP.cpp
+++ shortest_path_or_dist/02.5 IMP.cpp
@@ -1,3 +1,6 @@
 // Whenever we have UNdirected graph with a -ive weight
 // We can NEVER find shortest dist
 // BOTH dijkstra, Bellman Ford DO NOT work
+
+//if we have -ive edge bw u and v
+//then u and/or v will always be in the queue/priority_queue => TLE
