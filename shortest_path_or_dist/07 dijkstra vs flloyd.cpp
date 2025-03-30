3dc481089a2fce6123c7398d7bdae41383f9a4ef
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-31 01:39:05 +0530

    Update 07 dijkstra vs flloyd.cpp

--- shortest_path_or_dist/07 dijkstra vs flloyd.cpp
+++ shortest_path_or_dist/07 dijkstra vs flloyd.cpp
@@ -1,10 +1,10 @@
 // same problem statement as Flloyd warshall => 
 // find the shortest distances between every pair of vertices
 
-// If the graph has a -ive cycle: 
+// If the graph has a -ive edges: 
 // Dijkstra’s => TLE 
 // Use flloyd warshall
 
-// If the graph does NOT contain a -ive cycle: 
+// If the graph does NOT contain a -ive edges: 
 // Apply Dijkstra’s algorithm for every possible node
 // time complexity for Dijkstra’s algorithm for this purpose => O(V*(E*logV)) => lesser than Flloyd warshall
