052f7f76602ee2bf6d03fba55e21b04d24cf8f6a
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-31 00:41:49 +0530

    Update 06 Floyd Warshall Algorithm.cpp

--- shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
+++ shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
@@ -10,8 +10,86 @@
 // now it'll work
 
 
-// The graph is represented as an adjacency matrix of size n*n. 
+// The graph is represented as an adjacency MATRIX of size n*n. 
 // Matrix[i][j] denotes the weight of the edge from i to j. 
 // If Matrix[i][j]=-1, it means there is no edge from i to j.
 // Do it in place.
 
+// Algo:
+// matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
+// i = source node, j = destination node, k = the node via which we are reaching from i to j.
+// calculate dist[i][j] for every possible node k (k = 0, 1, ...V), where V = no. of nodes
+
+// Note: dist[i][i] = 0 (dist from itself)
+
+// How to detect a -ive cycle:
+// we discussed: cost of reaching a node from itself must be 0 (dist[i][i]=0). 
+// But in -ive cycle, if we try to reach a node from itself => the cost becomes < 0. 
+// This is ONLY possible if the graph contains a -ive cycle.
+// So, if we find that the cost of reaching any node from itself is < 0 => graph has a negative cycle.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+	void shortest_distance(vector<vector<int>>&matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;  //~INFINITE if no edge exists
+				}
+				if (i == j) 
+          matrix[i][j] = 0;  //dist from itself
+			}
+		}
+
+		for (int k = 0; k < n; k++) {  //VVIMP => K wala loop bahar
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+        if (i==j && dist[i][j]<0)
+          cout<<"detected a -ive wt cycle"<<endl;  //cycle detect
+        
+				if (matrix[i][j] == 1e9) //reset to INFINITY
+					matrix[i][j] = -1;
+			}
+		}
+    
+	}
+};
+
+
+int main() {
+
+	int V = 4;
+	vector<vector<int>> matrix(V, vector<int>(V, -1));
+	matrix[0][1] = 2;
+	matrix[1][0] = 1;
+	matrix[1][2] = 3;
+	matrix[3][0] = 3;
+	matrix[3][1] = 5;
+	matrix[3][2] = 4;
+
+	Solution obj;
+	obj.shortest_distance(matrix);
+
+	for (auto row : matrix) {
+		for (auto cell : row) {
+			cout << cell << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
